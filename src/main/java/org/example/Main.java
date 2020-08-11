b2ab67dd7225c972d3590a9a9d8d88ffa9629f50
SyringaVulgaris <madalinaionescu87@gmail.com>
2020-08-11 16:16:34 +0300

    added new overloaded methods to the project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,62 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
+        int num1 = 60;
+        int num2 = 70;
+        int num3 = 15;
+
         Numbers results = new Numbers();
 
-        int displaySum = results.sum(60, 70);
+        int displaySum = results.sum(num1, num2);
         System.out.println("The sum is: " + displaySum);
+        double displaySum2 = results.sum(60.5, 70.5);
+        System.out.println("The overloaded sum is: " + displaySum2);
 
         int displayMultiplication = results.multiplication(60, 70);
         System.out.println("The multiplication result is: " + displayMultiplication);
+        double displayMultiplication2 = results.multiplication(60.5, 70.5);
+        System.out.println("The overloaded multiplication2 is: " + displayMultiplication2);
 
         double displayDivision = results.division(60.0, 70.0);
         System.out.println("The division result is: " + displayDivision);
+        int displayDivision2 = results.division(60, 70);
+        System.out.println("The overloaded division2 result is: " + displayDivision2);
 
         int displaySubtraction = results.subtraction(60, 70);
         System.out.println("The subtraction result is: " + displaySubtraction);
+        double displaySubtraction2 = results.subtraction(60.5, 70.5);
+        System.out.println("The overloaded subtraction2 result is: " + displaySubtraction2);
 
         int displayDiverse = results.diverse(60, 70, 15);
         System.out.println("The diverse result is: " + displayDiverse);
+        float displayDiverseA = results.diverse(60.5f, 70.5f, 15.5f);
+        System.out.println("The overloaded diverseA result is: " + displayDiverseA);
 
         int displayDiverse1 = results.diverse1(60, 70, 15);
         System.out.println("The diverse1 result is: " + displayDiverse1);
+        double displayDiverse1A = results.diverse1(60.5, 70.5, 15.5);
+        System.out.println("The overloaded diverse1A result is: " + displayDiverse1A);
 
         double displayDiverse2 = results.diverse2(60, 70, 15);
         System.out.println("The diverse2 result is: " + displayDiverse2);
+        int displayDiverse2A = results.diverse2(60, 70, 15);
+        System.out.println("The overloaded diverse2A result is: " + displayDiverse2A);
 
         double displayDiverse3 = results.diverse3(60, 70, 15);
         System.out.println("The diverse3 result is: " + displayDiverse3);
+        int displayDiverse3A = results.diverse3(60, 70, 15);
+        System.out.println("The overloaded diverse3A result is: " + displayDiverse3A);
 
         double displayDiverse4 = results.diverse4(60, 70, 15, 5);
         System.out.println("The diverse4 result is: " + displayDiverse4);
+        int displayDiverse4A = results.diverse4(60, 70, 15, 5);
+        System.out.println("The overloaded diverse4A result is: " + displayDiverse4A);
 
         int displayDiverse5 = results.diverse5(60, 70, 15);
         System.out.println("The diverse5 result is: " + displayDiverse5);
+        double displayDiverse5A = results.diverse5(60.5, 70.5, 15.5);
+        System.out.println("The overloaded diverse5A result is: " + displayDiverse5A);
     }
-
-
 }
--- src/main/java/org/example/Numbers.java
+++ src/main/java/org/example/Numbers.java
@@ -7,13 +7,28 @@ public class Numbers {
         return result;
     }
 
+    public double sum(double a, double b) {
+        double result = a + b;
+        return result;
+    }
+
     public int multiplication(int a, int b) {
         int result = a * b;
         return result;
     }
 
+    public double multiplication(double a, double b) {
+        double result = a * b;
+        return result;
+    }
+
     public double division(double a, double b) {
         double result = a / b;
+        return result;
+    }
+
+    public int division(int a, int b) {
+        int result = a / b;
         return result;
     }
 
@@ -23,9 +38,21 @@ public class Numbers {
         return result;
     }
 
+    public double subtraction(double a, double b) {
+
+        double result = a - b;
+        return result;
+    }
+
     public int diverse(int a, int b, int c) {
 
         int result = (a + b) - c;
+        return result;
+    }
+
+    public float diverse(float a, float b, float c) {
+
+        float result = (a + b) - c;
         return result;
     }
 
@@ -35,9 +62,21 @@ public class Numbers {
         return result;
     }
 
+    public double diverse1(double a, double b, double c) {
+
+        double result = (a - b) * c;
+        return result;
+    }
+
     public double diverse2(double a, double b, double c) {
 
         double result = (a + b) / c;
+        return result;
+    }
+
+    public int diverse2(int a, int b, int c) {
+
+        int result = (a + b) / c;
         return result;
     }
 
@@ -46,13 +85,28 @@ public class Numbers {
         return result;
     }
 
+    public int diverse3(int a, int b, int c) {
+        int result = a + (b / c);
+        return result;
+    }
+
     public double diverse4(double a, double b, double c, double d) {
         double result = (a + b - c) / d;
         return result;
     }
 
+    public int diverse4(int a, int b, int c, int d) {
+        int result = (a + b - c) / d;
+        return result;
+    }
+
     public int diverse5(int a, int b, int c) {
         int result = (a - c) - b;
+        return result;
+    }
+
+    public double diverse5(double a, double b, double c) {
+        double result = (a - c) - b;
         return result;
     }
 }
